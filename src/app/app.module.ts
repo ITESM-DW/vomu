85cbcbc490231d1496f22a8fc657a8330802e0df
Emilio Cantón <eecanton@gmail.com>
2020-03-27 15:30:41 -0600

    Add translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// Translation
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-MX';
+import localeEsExtra from '@angular/common/locales/extra/es-MX';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,9 +14,10 @@ import { LandingComponent } from './vomu/landing/landing.component';
 import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './vomu/login/login.component';
 import { AuthService } from './auth.service';
-import { FormsModule } from '@angular/forms';
 import { ProfileComponent } from './vomu/profile/profile.component';
 import { AuthGuard } from './auth-guard.service';
+
+registerLocaleData(localeEs, 'es-MX', localeEsExtra);
 
 @NgModule({
   declarations: [
