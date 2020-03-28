6f2df5f900e4ce1beec9736947ea416e8cda8c90
SamBarcoM <samybarco@gmail.com>
2020-03-27 18:51:30 -0600

    write component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './vomu/login/login.component';
 import { AuthGuard } from './auth-guard.service';
 import { ProfileComponent } from './vomu/profile/profile.component';
 import { QualityComponent } from './vomu/quality/quality.component';
+import { WriteComponent } from './vomu/write/write.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile/:id', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: 'quality', component: QualityComponent }
+  { path: 'quality', component: QualityComponent },
+  { path: 'write', component: WriteComponent }
 ];
 
 @NgModule({
