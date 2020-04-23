e7a35b36b4248b656efbdfa7d6ac825d19180b71
Emilio Cantón <eecanton@gmail.com>
2020-04-23 15:14:25 -0500

    Add Components
    Co-authored-by: Christian Aguilar <christian_aguilar1994@hotmail.com>
    Co-authored-by: SamBarcoM <samybarco@gmail.com>
    Co-authored-by: Kyung Tak Woo <kevinwkt1997@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Vomu
 
+## To Do
+
+- [ ] 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FootbarComponent } from './components/footbar/footbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { CardComponent } from './components/card/card.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { QualityComponent } from './pages/quality/quality.component';
+import { ItemlistComponent } from './components/itemlist/itemlist.component';
+import { ApprovalComponent } from './pages/approval/approval.component';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileInputComponent } from './components/profile-input/profile-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FootbarComponent,
+    CarouselComponent,
+    JumbotronComponent,
+    CardComponent,
+    LoginComponent,
+    HomeComponent,
+    FeedComponent,
+    QualityComponent,
+    ItemlistComponent,
+    ApprovalComponent,
+    ProfileInfoComponent,
+    ProfileComponent,
+    ProfileInputComponent
   ],
   imports: [
     BrowserModule
