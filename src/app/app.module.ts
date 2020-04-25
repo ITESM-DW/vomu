ddd660d65c0f418abcdf9c9aa796c8a305fbf9bd
Emilio Cantón <eecanton@gmail.com>
2020-04-24 23:26:17 -0500

    Fully functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FootbarComponent } from './components/footbar/footbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { CardComponent } from './components/card/card.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
+import { ProfileInputComponent } from './components/profile-input/profile-input.component';
+import { ItemlistComponent } from './components/itemlist/itemlist.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { QualityComponent } from './pages/quality/quality.component';
-import { ItemlistComponent } from './components/itemlist/itemlist.component';
 import { ApprovalComponent } from './pages/approval/approval.component';
-import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { ProfileInputComponent } from './components/profile-input/profile-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FootbarComponent,
-    CarouselComponent,
-    JumbotronComponent,
     CardComponent,
-    LoginComponent,
+    CarouselComponent,
+    FooterComponent,
     HomeComponent,
+    ItemlistComponent,
+    JumbotronComponent,
+    NavbarComponent,
+    LoginComponent,
+    ProfileInfoComponent,
+    ProfileInputComponent,
+    ProfileComponent,
     FeedComponent,
     QualityComponent,
-    ItemlistComponent,
-    ApprovalComponent,
-    ProfileInfoComponent,
-    ProfileComponent,
-    ProfileInputComponent
+    ApprovalComponent
   ],
   imports: [
     BrowserModule
