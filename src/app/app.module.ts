307a3806c6c90e5ff48d83a004adf918051c7b61
ChrisAAP <christian_aguilar1994@hotmail.com>
2020-04-29 18:52:01 -0500

    Finished the user profile component with its scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { QualityComponent } from './pages/quality/quality.component';
 import { ApprovalComponent } from './pages/approval/approval.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { ApprovalComponent } from './pages/approval/approval.component';
     ProfileComponent,
     FeedComponent,
     QualityComponent,
-    ApprovalComponent
+    ApprovalComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
