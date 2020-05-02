2a1082363e816828bf07d3c7810609807c57edca
Emilio Cantón <eecanton@gmail.com>
2020-05-02 13:14:33 -0500

    hotfix: Ng serve working now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,45 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { CardComponent } from './components/card/card.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ProfileInfoComponent } from './components/profile-info/profile-info.component';
-import { ProfileInputComponent } from './components/profile-input/profile-input.component';
-import { ItemlistComponent } from './components/itemlist/itemlist.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { FeedComponent } from './pages/feed/feed.component';
-import { QualityComponent } from './pages/quality/quality.component';
-import { ApprovalComponent } from './pages/approval/approval.component';
-=======
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
-    CardComponent,
-    CarouselComponent,
-    FooterComponent,
-    HomeComponent,
-    ItemlistComponent,
-    JumbotronComponent,
-    NavbarComponent,
-    LoginComponent,
-    ProfileInfoComponent,
-    ProfileInputComponent,
-    ProfileComponent,
-    FeedComponent,
-    QualityComponent,
-    ApprovalComponent
-=======
-
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule
