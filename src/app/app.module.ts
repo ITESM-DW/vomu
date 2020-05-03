db3573c14ca22a658cd3a6467b97c3f9ba4fddc7
kevinwkt <kevinwkt1997@gmail.com>
2020-05-03 13:59:39 -0500

    feat: create stats charts and mock info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatsComponent } from './pages/stats/stats.component';
+
+// External modules.
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StatsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
