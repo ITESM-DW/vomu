b25ed4a78eec04bd39a0a04828f18e5ac64f98ba
SamBarcoM <samybarco@gmail.com>
2020-05-03 13:18:52 -0500

    Login modifications
    The component changes log in <-> sign in

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  register: boolean = false;
+  loginRegister: string = "Log In";
+  changeOption: string = "I don't have an account";
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  changeRegister() { 
+  		if(this.register == true) 
+  		{ 
+  			this.register = false; 
+  			this.loginRegister = "Log In";
+      		this.changeOption = "I don't have an account";
+  		}
+      	else 
+      	{ 
+      		this.register = true; 
+      		this.loginRegister = "Register";
+      		this.changeOption = "I already have an account";
+      	}
+      console.log("Variable changed");
+   }  
 }
