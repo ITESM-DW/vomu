ac3d6aeac39f51f350e30db6b2e531d908ea372d
kevinwkt <kevinwkt1997@gmail.com>
2020-05-02 22:10:04 -0500

    feat: added new components with inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseViewVideoComponent } from './components/course-view-video/course-view-video.component';
+import { CourseViewDetailsComponent } from './components/course-view-details/course-view-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseViewVideoComponent,
+    CourseViewDetailsComponent,
   ],
   imports: [
     BrowserModule
