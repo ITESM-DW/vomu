fb07cc51f2b3fc537cde5babfc928f6c5386e736
SamBarcoM <samybarco@gmail.com>
2020-05-03 01:06:29 -0500

    feat: course outline and cards created
    Components for course cards and outline created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseOutlineComponent } from './components/course-outline/course-outline.component';
+import { CourseCardsComponent } from './components/course-cards/course-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseOutlineComponent,
+    CourseCardsComponent,
   ],
   imports: [
     BrowserModule
