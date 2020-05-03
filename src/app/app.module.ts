dc8d91908e4ac14210db275d0a55832c99d90c1d
SamBarcoM <samybarco@gmail.com>
2020-05-03 02:02:23 -0500

    feat: user profile edit
    Temporal ngIf to shift between profile description and edit, user-profile-edit component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserProfileDescriptionComponent } from './components/user-profile-description/user-profile-description.component';
+import { UserProfileEditComponent } from './components/user-profile-edit/user-profile-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
     UserProfileDescriptionComponent,
+    UserProfileEditComponent,
   ],
   imports: [
     BrowserModule
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -11,6 +11,8 @@ export class UserProfileComponent implements OnInit {
 
   @Input() user: UserModel;
 
+  editBtn:number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -21,6 +23,11 @@ export class UserProfileComponent implements OnInit {
       description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem deleniti molestiae officiis optio? Facilis aliquam aut dicta impedit autem neque ut cumque, praesentium eveniet quasi accusantium blanditiis. Ipsam, aspernatur consequatur.",
       image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
     }
+  }
+
+  changeEditBtn():void{
+    if(this.editBtn==0) { this.editBtn=1; }
+    else { this.editBtn = 0; }
   }
 
 }
