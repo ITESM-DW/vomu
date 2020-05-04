5c64c921196e5e19d90b49219078014f7c09d544
Emilio Cantón <eecanton@gmail.com>
2020-05-04 01:45:53 -0500

    feat: Add student modul routing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,17 +8,9 @@ import { UserProfileEditComponent } from './user-profile-edit/user-profile-edit.
 import { StatsComponent } from './stats/stats.component';
 import { ChartsModule } from 'ng2-charts';
 
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { UserProfileDescriptionComponent } from './user-profile-description/user-profile-description.component';
-import { UserProfileEditComponent } from './user-profile-edit/user-profile-edit.component';
-import { GenericCardComponent } from './card-view/generic-card/generic-card.component';
-import { CardViewComponent } from './card-view/card-view.component';
-import { StatsComponent } from './stats/stats.component';
-import { ChartsModule } from 'ng2-charts';
-
 
 @NgModule({
-  declarations: [
+	declarations: [
 		UserProfileDescriptionComponent,
 		UserProfileEditComponent,
 		UserProfileComponent,
