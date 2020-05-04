ded5d918adff0ba7457fdb0c81de519223b9d152
Emilio Cantón <eecanton@gmail.com>
2020-05-03 23:01:44 -0500

    feat: Add student module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserProfileDescriptionComponent } from './user-profile-description/user-profile-description.component';
+import { UserProfileEditComponent } from './user-profile-edit/user-profile-edit.component';
+import { GenericCardComponent } from './card-view/generic-card/generic-card.component';
+import { CardViewComponent } from './card-view/card-view.component';
+import { StatsComponent } from '../components/stats/stats.component';
+import { ChartsModule } from 'ng2-charts';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		UserProfileDescriptionComponent,
+		UserProfileEditComponent,
+		UserProfileComponent,
+		GenericCardComponent,
+		CardViewComponent,
+		StatsComponent
+	],
+	imports: [
+		CommonModule,
+		ChartsModule
+	],
+	exports: [
+		UserProfileDescriptionComponent,
+		UserProfileEditComponent,
+		UserProfileComponent,
+		GenericCardComponent,
+		CardViewComponent,
+		StatsComponent
+	]
 })
 export class SharedModule { }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,18 @@
-import { UserModel } from './models/UserModel';
+import { UserModel, UserType } from './models/UserModel';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class UserService {
-	users: UserModel[] = [];
+	users: UserModel[] = [
+		{
+			email: 'string',
+			password: 'string',
+			type: UserType.STUDENT,
+			description: 'string',
+			image: 'https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=656&q=80',
+			courses: []
+		}
+	];
 
 	getUserInfo() {
 		return this.users[0];
