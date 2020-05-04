bc8bf0a6199bea50ce84bba3b4c766202aa5708c
kevinwkt <kevinwkt1997@gmail.com>
2020-05-03 23:30:11 -0500

    feat: generic table component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ExportFilesComponent } from './export-files/export-files.component';
+import { GenericTableComponent } from './generic-table/generic-table.component';
 
 @NgModule({
-  declarations: [ExportFilesComponent, AdminDashboardComponent],
+  declarations: [ExportFilesComponent, AdminDashboardComponent, GenericTableComponent],
   imports: [
     CommonModule,
     SharedModule,
