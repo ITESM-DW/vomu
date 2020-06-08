7f06c21bbe21e045af7e5b8ae4b4ee657bde82f8
Emilio Cantón <eecanton@gmail.com>
2020-06-08 18:12:45 -0500

    fix: Fixes #48

--- src/app/modules/user/models/ChartModel.ts
+++ src/app/modules/user/models/ChartModel.ts
@@ -2,15 +2,15 @@ import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label, SingleDataSet, MultiDataSet } from 'ng2-charts';
 
 export class ChartModel {
-    title: string;
+	title: string;
 
-    // Representation of data one or the other.
-    datasets?: ChartDataSets[] | SingleDataSet;
-    data?: MultiDataSet | SingleDataSet;
+	// Representation of data one or the other.
+	datasets?: ChartDataSets[] | SingleDataSet;
+	data?: MultiDataSet | SingleDataSet;
 
-    type: ChartType;
-    labels: Label[];
-    options?: ChartOptions;
-    legend?: boolean;
-    plugins?: any;
+	type: ChartType;
+	labels: Label[];
+	options?: ChartOptions;
+	legend?: boolean;
+	plugins?: any;
 }
