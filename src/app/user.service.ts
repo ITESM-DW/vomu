c226f1ecae909225b1923af98f3b5884fcdc051d
Emilio Cantón <eecanton@gmail.com>
2020-06-12 17:25:06 -0500

    feat: Add deployment

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -64,7 +64,7 @@ export class UserService {
 				return userM;
 			}
 		} catch(error){
-			console.log('fuck me sideways');
+			console.log(error);
 		}
 	}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
 	production: true,
 };
+
+export const baseUrl = 'https://vomu-backend.herokuapp.com/';
